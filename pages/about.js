25afe13746bc234ce05f134806995a6fc3ef5e3c
manish9427 <manish119427@gmail.com>
2025-06-16 01:04:59 +0530

    component folder

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 export default function About() {
+    // Correct this line to use the correct syntax for useEffect
+    useEffect(() => {
+        // This effect runs once when the component mounts
+        console.log("About component mounted");
+    }
+    , []);
+
   return (
     <div>
       <h1>About Us</h1>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import About from "./about";
 export default function Home() {
     return(
         <div>
-            <h1>Welcome to My Website</h1>
             <About/>
         </div>
     )
