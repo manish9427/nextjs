d830f44dbd54defdacafc7c6dc24bd0560b7b11b
manish9427 <manish119427@gmail.com>
2025-06-15 23:45:59 +0530

    pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import About from "./about";
+
+export default function Home() {
+    return(
+        <div>
+            <h1>Welcome to My Website</h1>
+            <About/>
+        </div>
+    )
+}
