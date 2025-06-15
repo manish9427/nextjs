f4ff1e109e365be7908c2971794975717cd16365
manish9427 <manish119427@gmail.com>
2025-06-16 01:22:19 +0530

    header

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import About from "./about";
+import Headers from "../components/Header";
 
 export default function Home() {
     return(
         <div>
+            <Headers/>
             <About/>
         </div>
     )
