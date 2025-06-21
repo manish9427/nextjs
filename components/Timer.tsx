91bd18895b2f9c13b6149189e3bce95005fdeafd
manish9427 <manish119427@gmail.com>
2025-06-22 00:43:40 +0530

    Timer

---
+++ components/Timer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState,useRef } from 'react';
+
+const Timer : React.FC = () => {
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+    
+    const startTimer = () => {
+    if (!intervalRef.current) {
+        intervalRef.current = setInterval(() => {
+         setTime(prev => prev + 1);
+        }, 1000);
+    }
+    };
+    const pauseTimer = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    };
+    const ResetTimer = () => {
+        setTime(0);
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    };
+
+    return (
+      <div>
+        <div>{time}</div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={pauseTimer}>Pause</button>
+        <button onClick={ResetTimer}>Reset</button>
+      </div>  
+    )
+}
+
+export default Timer;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import About from "./about";
 import Headers from "../components/Header";
+import Timer from "../components/Timer";
 
 export default function Home() {
     return(
         <div>
             <Headers/>
+            <Timer/>
             <About/>
         </div>
     )
