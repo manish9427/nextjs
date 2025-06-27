e017f6ebb42e45f3e1e00a31486a62819d106b27
manish9427 <manish119427@gmail.com>
2025-06-28 00:55:04 +0530

    new

---
+++ components/Slider.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Slider = () => {
+  const [value, setValue] = useState(50);
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setValue(Number(event.target.value));
+  };
+
+  return (
+    <div >
+      <h2 >
+        Slider Value: {value}
+      </h2>
+      <input
+        type="range"
+        // min="0"
+        // max="100"
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Slider;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import About from "./about";
 import Header from "./header";
 import Timer from "../components/Timer";
 import Counter from "./counter";
+import Slider from "../components/Slider"
 
 export default function Home() {
     return(
@@ -9,6 +10,7 @@ export default function Home() {
             <About/>
             <Header/>
             <Timer/>
+            <Slider/>
             <Counter/>
         </div>
     )
