65b6cf1b266d40cae590dbc5086a4625c1c6daa5
manish9427 <manish119427@gmail.com>
2025-07-15 14:51:54 +0530

    element

--- JavaScript/uniqueElement.js
+++ JavaScript/uniqueElement.js
@@ -1,25 +1,21 @@
-const arr  = [1,1,2,4,5];
-const obj = {};
+const arr  = [1, 2, 3, 4, 5, 1, 2, 3];
+
 const a = () => {
+    const obj = {};
     arr.forEach((item) => {
         if (obj[item]) {
         obj[item] += 1;
-        } else {
-        obj[item] = 1;
+    } else {
+            obj[item] = 1;
         }
     });
+    
+    
     
     // unique value
     for (const key in obj) {
         if (obj[key] === 1) {
             console.log(arr)
-        }
-    }
-
-    // duplicate value
-    for(let arr of Object.keys(obj)) {
-        if (obj[arr] > 1) {
-            console.log(`Number ${arr} appears ${obj[arr]} times`);
         }
     }
 }
