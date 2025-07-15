b74781804d93c0a71473ca2d703327e402c0c83e
manish9427 <manish119427@gmail.com>
2025-07-15 15:56:10 +0530

    button style

--- components/Calculator/Calculator.jsx
+++ components/Calculator/Calculator.jsx
@@ -4,14 +4,6 @@ import "./Calculator.css";
 const Calculator = () => {
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
-  const buttonStyle = {
-    padding: "15px",
-    fontSize: "18px",
-    cursor: "pointer",
-    border: "1px solid #ccc",
-    background: "#f9f9f9",
-    borderRadius: "5px",
-  };
 
   const calculate = () => {
     try {
@@ -28,43 +20,43 @@ const Calculator = () => {
   return (
     <div className="container">
       <h1>Calculator</h1>
-      <div>
+      <div className="inputField">
         <input type="text" value={input} />
       </div>
-      <div>Result:{input}</div>
+      <div className="result">Result:{input}</div>
       <div className="buttons">
         {["7", "8", "9", "/"].map((value) => (
           <button
+          className="buttonStyle"
             key={value}
             onClick={() => handleClick(value)}
-            style={buttonStyle}
           >
             {value}
           </button>
         ))}
         {["4", "5", "6", "*"].map((value) => (
           <button
+          className="buttonStyle"
             key={value}
             onClick={() => handleClick(value)}
-            style={buttonStyle}
           >
             {value}
           </button>
         ))}
         {["1", "2", "3", "-"].map((value) => (
           <button
+          className="buttonStyle"
             key={value}
             onClick={() => handleClick(value)}
-            style={buttonStyle}
           >
             {value}
           </button>
         ))}
         {["0", ".", "=", "+"].map((value) => (
           <button
+          className="buttonStyle"
             key={value}
             onClick={value === "=" ? calculate : () => handleClick(value)}
-            style={buttonStyle}
           >
             {value}
           </button>
