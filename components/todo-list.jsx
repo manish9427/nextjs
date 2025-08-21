a3f026ffba0010362d1958343344ff87f5a4a194
manish9427 <manish119427@gmail.com>
2025-08-21 21:45:02 +0530

    new

--- components/todo-list.jsx
+++ components/todo-list.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
 const TodoList = () => {
   return (
-    <div>Todo List</div>
-  )
-}
+    <div className="todo-list">
+      <div>
+        <h1>Add Item</h1>
+      </div>
+      <div>
+        <h1>Item List</h1>
+      </div>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
