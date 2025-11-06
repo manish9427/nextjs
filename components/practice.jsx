529f24f41e671f08242a16c4fbd5fb35027435f4
manish9427 <manish119427@gmail.com>
2025-11-06 18:35:28 +0530

    practice

--- components/practice.jsx
+++ components/practice.jsx
@@ -1,30 +1,28 @@
 import React,{useState,useEffect} from 'react'
 
-const Practice = ({setState}) => {
+const Practice = ({parentData, setParentData}) => {
+
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(json => setState(json))
-    },[setState])
+        .then(res=>res.json())
+        .then(json=> setParentData(json))
+    },[setParentData])
+
+    console.log(parentData)
   return (
-    <div>practice</div>
+    <div>{parentData.map((item,index)=>(
+        <pre key={item.id}>{JSON.stringify(item.title)}</pre>
+    ))}</div>
   )
 }
 
-const ParentComponent = () => {
-    const [state,setState] = useState([]);
+const ParentComponent = () =>{
+    const [parentData,setParentData] = useState([]);
     return(
-        <>
-                <Practice setState={setState}/>
-        {
-            state.map((item,index)=>(
-                <p key={item.id || index}>{JSON.stringify(item)}</p>
-            ))
-        }
-        </>
-
+        <div>
+            <Practice parentData={parentData} setParentData={setParentData}/>
+        </div>
     )
 }
-
+  
 export default ParentComponent
-
