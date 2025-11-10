ab93bee836eaa86f8b683f1000be12caf3e00933
manish9427 <manish119427@gmail.com>
2025-11-10 18:35:40 +0530

    todo

--- components/practice.jsx
+++ components/practice.jsx
@@ -1,54 +1,35 @@
-import React, { useState, useEffect, use } from "react";
+import React,{useState} from 'react'
 
-const Practice = ({ parentData, setParentData }) => {
-  const [page, setPage] = useState(1);
-  const userPerPage = 5;
+const practice = () => {
+  const [item,setitem] = useState([]);
+  const [inputValue,setInputvalue] = useState('');
 
-  const totalPages = Math.ceil(parentData.length/userPerPage);
-  const lastIndex = page * userPerPage;
-  const firstIndex = lastIndex - userPerPage;
-  const users = parentData.slice(firstIndex, lastIndex);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((json) => setParentData(json));
-  }, [setParentData]);
-
-  const incrementPage = () =>{
-    if(page<totalPages){
-      setPage(prevPage => prevPage+1);
-    }
+  const handleInput = (e) =>{
+    setInputvalue(e.target.value)
   }
 
-    const decrementPage = () =>{
-    if(page>1){
-      setPage(prevPage => prevPage-1);
+  const addItem = () => {
+    if(inputValue.trim()){
+      setitem([...item,inputValue.trim()]);
+      setInputvalue('');
     }
   }
-
-  console.log(parentData);
   return (
     <div>
       <div>
-        {users.map((item, index) => (
-          <pre key={item.id}>{JSON.stringify(item.title)}</pre>
-        ))}
+        <input type="text" placeholder='Add Item' value={inputValue} onChange={handleInput} />
+        <button onClick={addItem}>Add Item</button>
       </div>
-      <button onClick= {incrementPage}>+</button>
-      <span>{page}</span>
-      <button onClick={decrementPage}>-</button>
+      <div>
+        <p>Item List</p>
+        {
+          item.map((item,index)=>(
+            <p key={index}>{item}</p>
+          ))
+        }
+      </div>
     </div>
-  );
-};
+  )
+}
 
-const ParentComponent = () => {
-  const [parentData, setParentData] = useState([]);
-  return (
-    <div>
-      <Practice parentData={parentData} setParentData={setParentData} />
-    </div>
-  );
-};
-
-export default ParentComponent;
+export default practice
