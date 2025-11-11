1d78194bc81a4d9d9081e8c2e04490024ba1a152
manish9427 <manish119427@gmail.com>
2025-11-11 14:32:50 +0530

    new

--- components/practice.jsx
+++ components/practice.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const practice = () => {
+  const [item,setItem]=useState([])
+  const [inputValue,setInputValue] = useState('');
+  const handleInput = (e) =>{
+    setInputValue(e.target.value)
+  }
+
+  const addItem =()=>{
+    if(inputValue.trim()){
+      setItem([...item,inputValue.trim()])
+      setInputValue('')
+    }
+  }
   return (
-    <div>practice</div>
+    <div>
+      <div>
+        <input type="text" placeholder='Add Item' value={inputValue} onChange={handleInput}   />
+        <button onClick={addItem}>Add</button>
+      </div>
+      <div>
+        <p>Item List</p>
+        {
+          item.map((item,index)=>(
+            <div key={index}>
+              <span>{item}</span>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
