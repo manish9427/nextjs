f9046422f3bf782443ab6a4079cd8abda4c05499
manish9427 <manish119427@gmail.com>
2025-11-13 22:50:03 +0530

    new

--- components/practice.jsx
+++ components/practice.jsx
@@ -1,69 +1,50 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const practice = () => {
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const dataPerPage = 5;
 
-  useEffect(()=>{
+  const totalPage = Math.ceil(data.length / dataPerPage);
+  const lastIndex = page * dataPerPage;
+  const firstIndex = lastIndex - dataPerPage;
+  const currentPage = data.slice(firstIndex, lastIndex);
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res=>res.json())
-    .then(data=>{setData(data)})
-  },[])
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const handleIncrement = () => {
+    if (lastIndex < data.length) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
 
   return (
-    <pre key={data.id}>{JSON.stringify(data,null,2)}</pre>
-  )
-}
+    // <pre key={data.id}>{JSON.stringify(data,null,2)}</pre>
+    <div>
+      <div>
+        {currentPage.map((item, index) => (
+          <pre key={index}>{item.title}</pre>
+        ))}
+      </div>
+      <div>
+        <button onClick={handleIncrement}>+</button>
+        <span>
+          {page}/{totalPage}
+        </span>
+        <button onClick={handleDecrement}>-</button>
+      </div>
+    </div>
+  );
+};
 
-export default practice
-
-// import React, { useEffect, useState } from "react";
-
-// const practice = () => {
-//   const [data, setData] = useState([]);
-//   const [page, setPage] = useState(1);
-//   const dataPerPage = 5;
-
-//   const totalPage = Math.ceil(data.length / dataPerPage);
-//   const lastIndex = page * dataPerPage;
-//   const firstIndex = lastIndex - dataPerPage;
-//   const currentData = data.slice(firstIndex, lastIndex);
-
-//   useEffect(() => {
-//     fetch("https://jsonplaceholder.typicode.com/posts")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setData(data);
-//       });
-//   }, []);
-
-//   const handleIncrement = () => {
-//     if(lastIndex<data.length)
-//     setPage(prev => prev+1)
-//   }
-
-//   const handleDecrement =() => {
-//     if(page>1){
-//       setPage(prev => prev-1)
-//     }
-//   }
-
-//   // console.log(data);
-//   return (
-//     <div>
-//       <h1>Data</h1>
-//       <div>
-//       {/* <pre key={currentData.id}>{JSON.stringify(data,null,2)}</pre> */}
-//         {currentData.map((item) => (
-//           <pre key={item.id}>{item.title}</pre>
-//         ))}
-//       </div>
-//       <div>
-//         <button onClick={handleIncrement}>+</button>
-//         <span>{page}/{totalPage}</span>
-//         <button onClick={handleDecrement}>-</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default practice;
+export default practice;
