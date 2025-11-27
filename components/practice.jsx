5022cc2a824df91209743c4d086658455e16ab87
manish9427 <manish119427@gmail.com>
2025-11-27 21:30:47 +0530

    new

--- components/practice.jsx
+++ components/practice.jsx
@@ -1,57 +1,29 @@
-import React, { useState } from "react";
+import React,{useEffect,useState} from 'react'
 
 const practice = () => {
-  const [item, setItem] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [data,setData] = useState([]);
 
-  const handleInput = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const addItem = () => {
-    if(!inputValue.trim()) return ;
-    setItem([...item, inputValue]);
-    setInputValue("");
-  };
-
-  const handleEdit = (index) =>{
-    const newInput = prompt("Edit Item", item[index])
-    const updateditem = [...item]
-    updateditem[index] = newInput
-    setItem(updateditem)
-  }
-
-   const handleRemove = (index) => {
-    const updateItem = item.filter((_,i)=>i!==index)
-    setItem(updateItem)
-   }
+  useEffect(()=>{
+     fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then(data => {
+      setData(data)
+      console.log(data)
+    })
+  },[])
   return (
+    // <pre key={data.id}>{JSON.stringify(data,null,2)}</pre>
     <div>
-      <div>
-        <input
-          type="text"
-          placeholder="Add item"
-          value={inputValue}
-          onChange={handleInput}
-          onKeyDown={(e) => e.key === "Enter" && addItem()}
-        />
-        <button onClick={addItem}>Add item</button>
-      </div>
 
-      <div>
-        <p>List of Item</p>
-        {
-          item.map((item,index)=>(
-            <div key={index}>
-              <span>{item}</span>
-              <button onClick={()=>handleEdit(index)}>Edit</button>
-              <button onClick={()=>handleRemove(index)}>Remove</button>
-            </div>
-          ))
-        }
-      </div>
+      {
+        data.map((item,index)=>(
+          <pre key={item.index}>
+            {item.title}
+          </pre>
+        ))
+      }
     </div>
-  );
-};
+  )
+}
 
-export default practice;
+export default practice
