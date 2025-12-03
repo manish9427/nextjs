c907922c9df00e1cbed465edadf4da53b7317194
manish9427 <manish119427@gmail.com>
2025-12-03 22:40:39 +0530

    practice todo

--- components/practice.jsx
+++ components/practice.jsx
@@ -2,46 +2,41 @@ import React,{useState} from 'react'
 
 const practice = () => {
   const [item,setItem] = useState([]);
-  const [inputValue,setInputValue] = useState('');
+  const [value, setvalue] = useState('')
 
-  const handleInput =(e)=>{
-    setInputValue(e.target.value)
+  const handleInput = (e) => {
+    setvalue(e.target.value)
   }
 
   const addItem = () => {
-    setItem([...item,inputValue]);
-    setInputValue('');
+    setItem([...item,value])
+    setvalue('');
   }
 
-  const handleEdit = (index) =>{
-    const newInput = prompt('Add the Value', item[index])
-    const updatedItems = [...item];
-    updatedItems[index] = newInput;
-    setItem(updatedItems);
+  const handleEdit =(index)=>{
+    const newValue = prompt("Add new Value", item[index])
+    const updatedValue = [...item]
+    updatedValue[index] = newValue
+    setItem(updatedValue)
   }
 
-  const handleRemove = (index) =>{
-    const updatedItems = item.filter((_, i) => i !== index);
-    setItem(updatedItems);
+  const handleRemove =(index)=>{
+    const updatedValue = item.filter((_,i)=>i!==index)
+    setItem(updatedValue)
   }
-
-  
   return (
     <div>
-      <div>
-      <input type="text" placeholder='Add item' value={inputValue} onChange={handleInput}/>
+      <input type="text" placeholder='Add item' value={value} onChange={handleInput} />
       <button onClick={addItem}>Add Item</button>
-
-      </div>
       <div>
         <p>Item List</p>
         {
           item.map((item,index)=>(
-            <div key={item.index}>
+            <div>
               <span>{item}</span>
               <button onClick={()=>handleEdit(index)}>Edit</button>
               <button onClick={()=>handleRemove(index)}>Remove</button>
-            </div>
+              </div>
           ))
         }
       </div>
