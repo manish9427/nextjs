cf0eb9aa435d67333674a989714aa53172360a58
manish9427 <manish119427@gmail.com>
2025-12-16 19:21:40 +0530

    new

--- JavaScript/interview/sort.js
+++
@@ -1,9 +0,0 @@
-const sortElement = (arr) => {
-    return arr.sort((a,b)=>(b-a))
-}
-
-const arr = [1,3,2,1,5];
-const ans = sortElement(arr)
-console.log(ans)
-
-
--- components/practice.jsx
+++ components/practice.jsx
@@ -1,44 +1,8 @@
-import React, { useState,useEffect } from 'react'
+import React,{useEffect,useState} from 'react'
 
 const practice = () => {
-  const [data,setData]  = useState([]);
-  const [page,setPage] = useState(1);
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(res => res.json())
-    .then(data =>{
-      console.log(data)
-      setData(data);
-    })
-  },[])
-  const dataPerPage = 5;
-  const totalPage = Math.ceil(data.length/dataPerPage)
-  const lastIndex = page*dataPerPage // 1*5 = 5
-  const firstIndex = lastIndex - dataPerPage; // 5 - 5 = 0
-  const currentData = data.slice(firstIndex,lastIndex)
-
-  const handleIncrement = () => {
-    if(page<totalPage){
-      setPage(prev => prev+1)
-    }
-  }
-  const handleDecrement = () => {
-    if(page>1){
-      setPage(prev => prev-1 )
-    }
-  }
   return (
-    <div>
-      {/* <pre id={data.id}>{JSON.stringify(data,null,2)}</pre> */}
-      {
-        currentData.map((item)=>(
-          <pre id={item.id}>{item.title}</pre>
-        ))
-      }
-      <button onClick={handleIncrement}>+</button>
-      <span>{page}/{totalPage}</span>
-      <button onClick={handleDecrement}>-</button>
-    </div>
+    <div>practice</div>
   )
 }
 
