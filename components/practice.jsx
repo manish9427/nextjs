e1c427d0e265d5f36197e1d02d4945ca6ebb98c1
manish9427 <manish119427@gmail.com>
2025-12-22 16:38:15 +0530

    practice

--- components/practice.jsx
+++ components/practice.jsx
@@ -3,36 +3,41 @@ import React,{useState,useEffect} from 'react'
 const practice = () => {
   const [data,setData] = useState([])
   const [page,setPage] = useState(1)
+
   useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch("https://jsonplaceholder.typicode.com/todos")
     .then(res=>res.json())
     .then(json=>{
       setData(json)
     })
-  })
+  },[])
+
   const dataPerPage = 5;
-  const lastIndex = page*dataPerPage
-  const firstIndex = lastIndex - dataPerPage;
+  const lastIndex = page*dataPerPage;
+  const firstIndex = lastIndex - dataPerPage
   const currentData = data.slice(firstIndex,lastIndex)
   const totalPage = Math.ceil(data.length/dataPerPage)
 
-  const handleIncrement = () =>{
-    if(page<totalPage)
-    setPage(prev=>prev+1)
+  const handleIncrement  =()=>{
+    if(page<totalPage){
+      setPage(prev=>prev+1)
+    }
   }
-  const handleDecrement = () =>{
+
+    const handleDecrement  =()=>{
     if(page>1){
       setPage(prev=>prev-1)
     }
   }
+
   return (
     <div>
-      {/* <pre key={data.id}>{JSON.stringify(data,null,2)}</pre> */}
       {
         currentData.map((item)=>(
           <pre key={item.id}>{item.title}</pre>
         ))
       }
+
       <button onClick={handleIncrement}>+</button>
       <span>{page}/{totalPage}</span>
       <button onClick={handleDecrement}>-</button>
@@ -40,4 +45,4 @@ const practice = () => {
   )
 }
 
-export default practice 
+export default practice
