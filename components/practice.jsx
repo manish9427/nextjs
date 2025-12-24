b1ce375409cdef477cfcc7a1fce0e10801d0e3c5
manish9427 <manish119427@gmail.com>
2025-12-24 15:15:01 +0530

    practice

--- components/practice.jsx
+++ components/practice.jsx
@@ -1,56 +1,48 @@
-import React, { useState } from "react";
+import React,{useState} from 'react'
 
 const practice = () => {
-  const [item, setItem] = useState([]);
-  const [value, setValue] = useState("");
+  const [item,setItem] = useState([])
+  const [value,setValue] = useState('')
 
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
+  const handleInput = (e)=>{
+    setValue(e.target.value)
+  }
 
-  const handleAddItem = () => {
-    if (value !== "") {
-      setItem([...item, value]);
-      setValue("");
+
+  const handleAddItem = () =>{
+    if(value!==""){
+      setItem([...item,value])
+      setValue('')
     }
-  };
+  }
+
+  const handleKeyDown =(e) =>{
+    if(e.key === "Enter"){
+      handleAddItem()
+    }
+  }
 
   const handleEdit =(index) =>{
-    const newValue = prompt('Add New Value', item[index])
-    if(newValue!==null && newValue!==""){
-
+    const newValue = prompt("Add new Value", item[index])
+    if(newValue !== null && newValue !== ""){
       const updatedValue = [...item]
       updatedValue[index] = newValue
       setItem(updatedValue) 
     }
   }
 
-  const handleRemove = (index)=>{
+  const handleRemove = (index) =>{
     const updatedValue = item.filter((_,i)=>i!==index)
     setItem(updatedValue)
   }
 
-  const hanldeKeyDown = (e)=>{
-    if(e.key === 'Enter'){
-      handleAddItem();
-    }
-  }
-
-
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Add item"
-        value={value}
-        onChange={handleInput}
-        onKeyDown={hanldeKeyDown}
-      />
+      <input type="text" placeholder='Add item' value={value} onChange={handleInput} onKeyDown={handleKeyDown} />
       <button onClick={handleAddItem}>Add Item</button>
-
       <div>
-        <p>List Added Item</p>
+        <p>List of Added Item</p>
         {
           item.map((item,index)=>(
             <div key={index}>
@@ -62,7 +54,7 @@ const practice = () => {
         }
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default practice;
+export default practice
