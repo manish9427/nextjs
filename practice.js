4fa340c9ff971ced3a12a0a6597692eb62d9932c
kish2026 <Dev@12345@1999>
2026-02-03 00:26:32 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,18 @@
+// 1. Write a function that returns only users older than 28 age
+// 2. Write a function that groups users by city
+//    Expected output: { "NYC": [...], "LA": [...] }
+const users = [
+  { id: 1, name: "Alice", age: 25, city: "NYC" },
+  { id: 2, name: "Bob", age: 30, city: "LA" },
+  { id: 3, name: "Charlie", age: 25, city: "NYC" },
+  { id: 4, name: "David", age: 35, city: "LA" }
+];
+
+console.log(users.id.age)
+
+// const fun = (users) =>{
+//     const userAge = users.filter((users)=>users.age>28)
+//     console.log(userAge) ;
+// }
+
+// fun()
