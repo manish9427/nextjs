06dcc16ddb29f5d28dba38af3e2db8fd0ccabf65
manish9427 <manish119427@gmail.com>
2026-02-19 21:47:37 +0530

    new

--- Frontend/script.js
+++
@@ -1,7 +0,0 @@
-  // document.getElementById("parent").addEventListener("click", () => {
-  //   console.log("Parent clicked");
-  // });
-
-  // document.getElementById("child").addEventListener("click", (e) => {
-  //   e.stopPropagation();
-  //   console.log("Child clicked")});
