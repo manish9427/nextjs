b38991061b24232d40c3ee4b4808459b0208a763
manish9427 <manish119427@gmail.com>
2026-05-06 15:32:20 +0530

    api

---
+++ Java/first.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your num: ");
+      int num = sc.nextInt();
+      System.out.println(num);
+      System.out.println("Enter your Word: "); // Manish Verma
+      String word = sc.next();
+      String sentence = sc.nextLine();
+      System.out.println(word); // Manish
+      System.out.println("Enter your Sentence: ");
+      System.out.println(sentence);
+    }
+}
