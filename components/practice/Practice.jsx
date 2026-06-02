454fdd3b91cb3946aea05bf9ee6c6f3166b4066d
Manish Verma <107556633+manish9427@users.noreply.github.com>
2026-06-02 07:52:02 +0000

    practice

--- components/practice/Practice.jsx
+++ components/practice/Practice.jsx
@@ -1,37 +1,11 @@
-"use client"
+"use client";
 
-import React,{useState,useEffect} from 'react'
+import React, { useState } from "react";
 
 const Practice = () => {
-  const [data,setData] = useState([]);
-  useEffect(()=>{
-    fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(json => {
-      setData(json.products)
-      console.log(json)
-    } )
-  },[])
+  const [data, setData] = useState("Manish");
 
-  const style = {
-    img : {
-      width:'30px',
-      height:'30px',
-    }
-  }
-  return (
-    <div>
-    {
-      data.map( (item) =>(
-        <div key={item.id}>
-        <p>{item.title}</p>
-        <img style={style.img} src={item.images[0]}/>
-        </div>
-      )
-      )
-    }
-    </div>
-  )
-}
+  return <>{data}</>;
+};
 
-export default Practice
+export default Practice;
